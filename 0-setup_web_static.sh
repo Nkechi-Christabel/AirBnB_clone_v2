10d850564a156afd14a19ae86b479adac1d10bb7
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-10 09:48:29 -0800

    Wrote a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,13 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/{releases/test,shared}
 sudo chown -R ubuntu:ubuntu /data/
-echo "<html><head><title>Test Page</title></head><body>Test content</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo -e "<html>
+<head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo sed -i "/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
 sudo service nginx restart
