be6c3fb0b576f51b10a4f6ed19910e19884dc6d9
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 10:42:46 -0800

    Wrote  a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,40 +4,67 @@ Based on the file 3-deploy_web_static.py, this script deletes out-of-date
 archives, using the function do_clean
 """
 
-from fabric.api import env, local, put, run
-from datetime import datetime
-from os.path import exists, isdir
-env.hosts = ['54.210.106.177', '54.174.70.150']
+
+def deploy():
+    ''' Deploys archive '''
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
 
 
-def local_clean(number=0):
-    """Local Clean"""
-    fd_list = local('ls -1t versions', capture=True)
-    fd_list = fd_list.split('\n')
-    n = int(number)
-    if n in (0, 1):
-        n = 1
-    print(len(fd_list[n:]))
-    for i in fd_list[n:]:
-        local('rm versions/' + i)
+def do_pack():
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
+    try:
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
+        return archive_path
+    except:
+        return None
 
 
-def remote_clean(number=0):
-    """Remote Clean"""
-    fd_list = run('ls -1t /data/web_static/releases')
-    fd_list = fd_list.split('\r\n')
-    print(fd_list)
-    n = int(number)
-    if n in (0, 1):
-        n = 1
-    print(len(fd_list[n:]))
-    for i in fd_list[n:]:
-        if i is 'test':
-            continue
-        run('rm -rf /data/web_static/releases/' + i)
+def do_deploy(archive_path):
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
+        return True
+    except:
+        return False
 
 
 def do_clean(number=0):
-    """Fabric script that deletes aout of dates archives"""
-    local_clean(number)
-    remote_clean(number)
+    ''' Removes out of date archives locally and remotely '''
+    number = int(number)
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions; ls -t | tail -n +{} | xargs rm -rf'
+          .format(number))
+    releases_path = '/data/web_static/releases'
+    run('cd {}; ls -t | tail -n +{} | xargs rm -rf'
+        .format(releases_path, number))
