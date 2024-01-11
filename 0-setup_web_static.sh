2e9d7770797b85e5519bfec1a2e9419d6dd0e348
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 02:15:33 -0800

    Updated the script that sets up the web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,6 +12,6 @@ echo "<html>
 </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
