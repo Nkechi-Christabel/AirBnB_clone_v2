75081b7e83b6c4d7adf56650bbda96c0ac9120dc
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 00:41:04 -0800

    Updated the script that sets up the web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # This script sets up your web servers for the deployment of web_static.
 
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo chown -R ubuntu:ubuntu /data/
