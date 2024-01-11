f35f197976619ad13a9da6391cec1925a21bc33a
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 05:08:05 -0800

    Updated the Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,5 +13,7 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+if ! grep -q "location /hbnb_static/ " /etc/nginx/sites-available/default; then
+    sudo sed -i "44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+fi
 sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,28 +3,41 @@
 # archive to your web servers, using the function do_deploy.
 from fabric.api import env, put, run, sudo
 from os import path
+from os.path import exists
 
 env.hosts = ['54.210.106.177', '54.174.70.150']
 
-
 def do_deploy(archive_path):
-    """Distributes an archive to web servers."""
-    if not path.exists(archive_path):
+    """
+    Distributes an archive to web servers
+    """
+    if not exists(archive_path):
         return False
 
     try:
-        filename = path.basename(archive_path).split(".")[0]
-        release_path = f"/data/web_static/releases/{filename}"
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
 
-        put(archive_path, "/tmp/")
+        # Extract the archive to /data/web_static/releases/<archive filename without extension>/
+        archive_filename = archive_path.split('/')[-1]
+        archive_name = archive_filename.split('.')[0]
+        release_path = '/data/web_static/releases/{}'.format(archive_name)
 
-        run(f"mkdir -p {release_path}")
-        run(f"tar -xzf /tmp/{path.basename(archive_path)} -C {release_path}")
-        run(f"rm /tmp/{path.basename(archive_path)}")
+        run('mkdir -p {}'.format(release_path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
 
-        sudo("rm -rf /data/web_static/current")
-        sudo(f"ln -s {release_path} /data/web_static/current")
+        # Remove the archive from the web server
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Delete the symbolic link /data/web_static/current
+        current_link = '/data/web_static/current'
+        run('rm -f {}'.format(current_link))
+
+        # Create a new symbolic link pointing to the new version of the code
+        run('ln -s {} {}'.format(release_path, current_link))
+
         return True
+
     except Exception as e:
-        print(f"Error deploying archive: {e}")
+        print(e)
         return False
