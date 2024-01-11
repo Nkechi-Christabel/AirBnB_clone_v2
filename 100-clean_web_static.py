20ffafbfce64d06981bec9cfe1c5ccdaf41867d2
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 10:47:10 -0800

    Wrote  a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy.

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,68 +3,20 @@
 Based on the file 3-deploy_web_static.py, this script deletes out-of-date
 archives, using the function do_clean
 """
-
-
-def deploy():
-    ''' Deploys archive '''
-    archive_path = do_pack()
-    if not archive_path:
-        return False
-    return do_deploy(archive_path)
-
-
-def do_pack():
-    '''
-    Generates a tgz archive from the
-    contents of the web_static folder
-    '''
-    try:
-        local('mkdir -p versions')
-        datetime_format = '%Y%m%d%H%M%S'
-        archive_path = 'versions/web_static_{}.tgz'.format(
-            datetime.now().strftime(datetime_format))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        print('web_static packed: {} -> {}'.format(archive_path,
-              os.path.getsize(archive_path)))
-        return archive_path
-    except:
-        return None
-
-
-def do_deploy(archive_path):
-    '''
-    Deploy archive to web server
-    '''
-    if not os.path.exists(archive_path):
-        return False
-    file_name = archive_path.split('/')[1]
-    file_path = '/data/web_static/releases/'
-    releases_path = file_path + file_name[:-4]
-    try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(releases_path))
-        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
-        run('rm -rf {}/web_static'.format(releases_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(releases_path))
-        print('New version deployed!')
-        return True
-    except:
-        return False
+env.hosts = ['54.210.106.177', '54.174.70.150']
 
 
 def do_clean(number=0):
-    ''' Removes out of date archives locally and remotely '''
-    number = int(number)
-    if number == 0:
-        number = 2
-    else:
-        number += 1
+    """Deletes out-of-date archives."""
 
-    local('cd versions; ls -t | tail -n +{} | xargs rm -rf'
-          .format(number))
-    releases_path = '/data/web_static/releases'
-    run('cd {}; ls -t | tail -n +{} | xargs rm -rf'
-        .format(releases_path, number))
+    # Clean local versions folder
+    try:
+        local("ls -t versions | tail -n +{number} | xargs rm -rf")
+    except Exception as e:
+        print(f"Error cleaning local archives: {e}")
+
+    # Clean remote releases folders
+    try:
+        run("ls -t /data/web_static/releases | tail -n +{number} | xargs rm -rf")
+    except Exception as e:
+        print(f"Error cleaning remote archives: {e}")
