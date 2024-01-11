a6e1216a19fe8b5f7f1a68398368c8b15e25a16e
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 06:51:10 -0800

    Updated the a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,26 +5,28 @@ archive to the web servers, using the function do_deploy.
 """
 from fabric.api import env, put, run, sudo
 from os import path
-from os.path import exists
 env.hosts = ['54.210.106.177', '54.174.70.150']
 
+
 def do_deploy(archive_path):
-    """deploy web static with fabric"""
-    if exists(archive_path) is False:
+    """Distributes an archive to web servers."""
+    if not path.exists(archive_path):
         return False
 
     try:
-        filename = archive_path.split("/")[-1]
-        no_excep = filename.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}{}/'.format(path, no_excep))
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
-        run('sudo rm /tmp/{}'.format(filename))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
-        run('sudo rm -rf {}{}/web_static'.format(path, no_excep))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
+        filename = path.basename(archive_path).split(".")[0]
+        release_path = f"/data/web_static/releases/{filename}"
+
+        print("Basename", path.basename(archive_path))
+        put(archive_path, "/tmp/")
+
+        run(f"mkdir -p {release_path}")
+        run(f"tar -xzf /tmp/{path.basename(archive_path)} -C {release_path}")
+        run(f"rm /tmp/{path.basename(archive_path)}")
+
+        sudo("rm -rf /data/web_static/current")
+        sudo(f"ln -s {release_path} /data/web_static/current")
+
         return True
-    except BaseException:
+    except Exception as e:
         return False
