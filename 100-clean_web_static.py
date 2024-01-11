6cf73956c9dc2fbc9739d70b539d2728af26d560
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 11:12:22 -0800

    Added a puppet that redo's the task #0 but by using Puppet

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,40 +3,22 @@
 Based on the file 3-deploy_web_static.py, this script deletes out-of-date
 archives, using the function do_clean
 """
-from fabric.api import env, run, local, lcd, cd
-from datetime import datetime
-import os
+from fabric.api import *
 
 env.hosts = ['54.210.106.177', '54.174.70.150']
+env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """
-    Deletes out-of-date archives
+    """ CLEANS """
 
-    Parameters:
-        number (int): Number of archives to keep
-    """
     number = int(number)
 
-    if number < 0:
-        number = 0
-    elif number == 1:
-        number = 1
+    if number == 0:
+        number = 2
     else:
-        number = number + 1
+        number += 1
 
-    with lcd('./versions'):
-        local('ls -t | tail -n +{} | xargs rm -f'.format(number))
-
-    with cd('/data/web_static/releases'):
-        run('ls -t | tail -n +{} | xargs rm -rf'.format(number))
-
-    with cd('/data/web_static/releases'):
-        run('ls -t | tail -n +{} | xargs rm -rf'.format(number))
-
-    print('Cleaned up old versions')
-
-
-if __name__ == "__main__":
-    do_clean()
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
