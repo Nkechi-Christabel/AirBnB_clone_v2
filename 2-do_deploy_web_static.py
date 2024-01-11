e75c32300be78679d3a585f8373b4a9ca797a12c
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 02:02:29 -0800

    Wrote Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Based on the file 1-pack_web_static.py, this script distributes an
+# archive to your web servers, using the function do_deploy.
+
+from fabric.api import env, put, run, sudo
+from os import path
+
+env.hosts = ['54.210.106.177', '54.174.70.150']
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers."""
+    if not path.exists(archive_path):
+        return False
+
+    try:
+        filename = path.basename(archive_path).split(".")[0]
+        release_path = f"/data/web_static/releases/{filename}"
+
+        put(archive_path, "/tmp/")
+
+        run(f"mkdir -p {release_path}")
+        run(f"tar -xzf /tmp/{path.basename(archive_path)} -C {release_path}")
+        run(f"rm /tmp/{path.basename(archive_path)}") 
+
+        sudo("rm -rf /data/web_static/current")
+        sudo(f"ln -s {release_path} /data/web_static/current")
+        return True
+    except Exception as e:
+        print(f"Error deploying archive: {e}")
+        return False
