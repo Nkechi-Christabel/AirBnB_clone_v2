ce2547dde743f6efa90d4453080fd0ddb7520b07
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-11 01:01:22 -0800

    Updated the script that sets up the web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,15 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo useradd ubuntu
 sudo chown -R ubuntu:ubuntu /data/
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>
+<head>
+</head>
+<body>
+    Hello World!
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo sed -i '44i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
