5848c6984a54ea3a73c131b9f2070f91941295d8
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-13 04:14:38 -0800

    Updated the keep it clean script..

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
 """
 Based on the file 1-pack_web_static.py, this script distributes an
 archive to the web servers, using the function do_deploy.
 """
-from fabric.api import local, env, put, run, sudo
+from fabric.api import env, put, run
 from os import path
 import os
 
-env.hosts = ['54.210.106.177', '54.174.70.150', '127.0.0.1']
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/id_rsa"
+env.hosts = ['54.210.106.177', '54.174.70.150']
 
 
 def do_deploy(archive_path):
