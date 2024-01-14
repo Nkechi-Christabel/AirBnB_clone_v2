b51257141668a0895b1ee2f445bddc1f798852e8
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-13 21:25:16 -0800

    Updated Deploy archive script.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,11 +3,11 @@
 Based on the file 1-pack_web_static.py, this script distributes an
 archive to the web servers, using the function do_deploy.
 """
-from fabric.api import local, env, put, run, sudo
+from fabric.api import env, put, run, sudo
 from os import path
 import os
 
-env.hosts = ['54.210.106.177', '54.174.70.150', '127.0.0.1']
+env.hosts = ['54.210.106.177', '54.174.70.150']
 
 
 def do_deploy(archive_path):
@@ -25,9 +25,9 @@ def do_deploy(archive_path):
         run(f"tar -xzf /tmp/{path.basename(archive_path)} -C {release_path}")
         run(f"rm /tmp/{path.basename(archive_path)}")
 
-        sudo(f"mv {release_path}/web_static/* {release_path}/")
-        sudo("rm -rf /data/web_static/current")
-        sudo(f"ln -s {release_path} /data/web_static/current")
+        run(f"mv {release_path}/web_static/* {release_path}/")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s {release_path} /data/web_static/current")
         return True
     except Exception as e:
         print(f"Error: {e}")
