1322ebb82245648216889c59d18842dc9e233ecb
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-26 05:16:09 -0800

    Wrote a script that starts a Flask web application, displaying n is a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -43,5 +43,13 @@ def python(text):
     return "Python {}".format(escape(text).replace('_', ' '))
 
 
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Display "n is a number" only if n is an integer
+    """
+    return "{} is a number".format(n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
