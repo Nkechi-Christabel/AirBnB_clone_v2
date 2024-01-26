6d22a2ac32cd340346bae3dfd4aa72656df80d47
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-01-26 12:05:16 +0000

    Wrote a script that starts a Flask web application.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,8 +12,8 @@ def hello_hbnb():
     """
     Returns: Hello HBNB string.
     """ 
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-     app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
